988b81cf47ad3bfb3996d75b45481c98ebda5e8f
mariovagomarzal <mariovagomarzal@gmail.com>
2024-09-05 23:53:41 +0200

    Rename the source code directory and create a `_utils` module

---
+++ bh_testing/_utils.py
@@ -0,0 +1,26 @@
+import numpy as np
+from PIL import Image
+
+
+def rotate_y(angle):
+    return np.array([
+        [np.cos(angle), 0, np.sin(angle)],
+        [0, 1, 0],
+        [-np.sin(angle), 0, np.cos(angle)]
+    ])
+
+def rotate_z(angle):
+    return np.array([
+        [np.cos(angle), -np.sin(angle), 0],
+        [np.sin(angle), np.cos(angle), 0],
+        [0, 0, 1]
+    ])
+
+def to_PIL_image(output_texture, size):
+    data = output_texture.read()
+    image = np.frombuffer(data, dtype=np.float32).reshape((size[1], size[0], 4))
+
+    # Convert to 8-bit per channel and create a PIL image
+    total_image = np.clip(image * 255, 0, 255).astype(np.uint8)
+    pil_image = Image.fromarray(total_image)
+    return pil_image
--- bh_testing/main.py
+++ bh_testing/main.py
@@ -1,9 +1,13 @@
 from objects import *
 
+
+def to_radians(degrees):
+    return degrees * np.pi / 180
+
 disk_texture = Image.open("assets/disk.png")
 bg_texture = Image.open("assets/grid.png")
 
-cam = Camera.orbit(angle_y=-5*DEGREES)
+cam = Camera.orbit(angle_y=to_radians(-5))
 bh = BlackHole()
 disk = Disk(texture=disk_texture)
 scene = Scene(cam, bh, disk, bg_texture)
