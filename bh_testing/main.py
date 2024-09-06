1f15787e432a482d17d8baa94d62f1be99de182c
mariovagomarzal <mariovagomarzal@gmail.com>
2024-09-06 20:46:48 +0200

    Add a `NotImplementedError` to the `main` module

--- bh_testing/main.py
+++ bh_testing/main.py
@@ -1,17 +1,5 @@
-from objects import *
-
-
-def to_radians(degrees):
-    return degrees * np.pi / 180
-
-disk_texture = Image.open("assets/disk.png")
-bg_texture = Image.open("assets/grid.png")
-
-cam = Camera.orbit(angle_y=to_radians(-5))
-bh = BlackHole()
-disk = Disk(texture=disk_texture)
-scene = Scene(cam, bh, disk, bg_texture)
-
-img = scene.render()
-img.show()
-img.save("bh.png")
+"""
+This modules contains a sample `main` function to render some example
+images.
+"""
+raise NotImplementedError("This module is not implemented yet.")
