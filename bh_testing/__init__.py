595190099c820fbea8394e5ef0b4b5b8bbb9550c
mariovagomarzal <mariovagomarzal@gmail.com>
2024-09-06 20:45:38 +0200

    Add the imports to `__init__.py` file

--- bh_testing/__init__.py
+++ bh_testing/__init__.py
@@ -5,7 +5,15 @@ __all__ = [
     "ORIENTED_BG",
     "ORANGE_DISK",
     "BlackHole",
+    "Camera",
+    "ShaderRenderer",
+    "BlackHoleRenderer",
+    "Scene",
 ]
 
 from .textures import Texture, PINK_BG, MULTICOLOR_BG, ORIENTED_BG, ORANGE_DISK
 from .black_hole import BlackHole
+from .camera import Camera
+from .shader_renderer import ShaderRenderer, BlackHoleRenderer
+from .scene import Scene
+
