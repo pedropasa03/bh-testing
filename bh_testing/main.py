d2d6cf8bab9c78a683a2cc47ac85cff504eda39a
pedropasa03 <pedropasa2003@gmail.com>
2024-09-08 01:06:12 +0200

    Add a `main` function

--- bh_testing/main.py
+++ bh_testing/main.py
@@ -2,4 +2,33 @@
 This modules contains a sample `main` function to render some example
 images.
 """
-raise NotImplementedError("This module is not implemented yet.")
+"""
+This modules contains a sample `main` function to render some example
+images.
+"""
+from bh_testing import *
+
+def main():
+    bh = BlackHole(
+        radius=5.0,
+        inner_radius=15.0,
+        outer_radius=40.0,
+        thickness=1.0,
+        texture=ORANGE_DISK
+    )
+
+    camera = Camera(
+        resolution=[1000,1000],
+        angle_y=0,
+        angle_z=0,
+        focal_length=1,
+        origin=[-100.0, 0.0, 0.0]
+    )
+    scene = BlackHoleScene(bh,camera, PINK_BG)
+
+    img = scene.render()
+    img.show()
+
+if __name__ == "__main__":
+    main()
+
